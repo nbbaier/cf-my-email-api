47789ca3399b0e431cb08d9451f8ddd1a6977926
Nico Baier <nico.baier@gmail.com>
2025-07-25 17:20:23 -0500

    added ref info to request logging

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import { Hono } from "hono";
 const app = new Hono();
 
 app.get("/", (c) => {
-	console.log("GET /");
+	const referer = c.req.header("Referer");
+	console.log(`GET / (from ${referer})`);
 	return c.text("Hello Hono!");
 });
 
 app.post("/", (c) => {
-	console.log("POST /");
+	const referer = c.req.header("Referer");
+	console.log(`POST / (from ${referer})`);
 	return c.text("Hello Hono!");
 });
 
